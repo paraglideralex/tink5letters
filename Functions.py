1a1b0c83fc645fbab13f312d436fd360a844a1ba
paraglideralex <alexvt1997@mail.ru>
2022-11-06 20:48:11 +0300

    прописал комменты к файлам

--- Functions.py
+++ Functions.py
@@ -1,4 +1,11 @@
 def InStr(FindLetters,StringIn):
+    '''
+    Проверяет вхождение в строку всех заданных букв в поисковой строке
+
+    :param FindLetters: string, буквы, наличие которых надо проверить
+    :param StringIn: string – строка, в которой надо проверить наличие букв из FindLetters
+    :return: bool – входят ли все буквы из поисковой строки, или нет
+    '''
     AllIn = True
     for letter in FindLetters:
         if not (letter in StringIn):
@@ -7,6 +14,12 @@ def InStr(FindLetters,StringIn):
     return AllIn
 
 def CheckDoubl(str):
+    '''
+    Проверяет наличие дубликатов в строке
+
+    :param str: string – входная строка для анализа на дубликаты
+    :return: bool – булево значение, есть дубликаты или нет
+    '''
     Ans = True
     for let in str:
         b = str.count(let)
